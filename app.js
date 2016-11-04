b037be4a92f523dfc6a1c6a9bc56710ba9b6ac16
MrMargouillat <klaja.raphael@gmail.com>
2016-11-04 20:06:47 +0100

    done 2

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 // Highlisht syntaxe
 SyntaxHighlighter.all()
-
-var n = parseInt(prompt("Entrez la valeur de n:"));
-var x = 0;
-var i = 1;
-while(n > 1)
-{
-    if(n * i >4)
+function  algo(){
+    var n = parseInt(prompt("Entrez la valeur de n:"));
+    var x = 0;
+    var i = 1;
+    while(n > 1)
     {
-        x = x + 2*n;
+        if(n * i >4)
+        {
+            x = x + 2*n;
+        }
+        else
+        {
+            x = x + n;
+        }
+        n = n -2;
+        i = i +1;
     }
-    else
-    {
-        x = x + n;
-    }
-    n = n -2;
-    i = i +1;
+    alert("La valeur de x est :" + x);
 }
-alert("La valeur de x est :" + x);
