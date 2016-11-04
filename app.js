e81ff135be7bd3b4ff55092a962e49c7169ff428
MrMargouillat <klaja.raphael@gmail.com>
2016-11-04 21:53:38 +0100

    adding 2nd DM

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Highlisht syntaxe
 SyntaxHighlighter.all()
-function  algo(){
+function  algo1(){
     var n = parseInt(prompt("Entrez la valeur de n:"));
     var x = 0;
     var i = 1;
@@ -18,4 +18,26 @@ function  algo(){
         i = i +1;
     }
     alert("La valeur de x est :" + x);
+}
+
+function  algo2(){
+    var x_sol = 0;
+    var y_sol = 0;
+    var benefice_max = 0;
+    for(var x = 0; x < 50; x++)
+    {
+        for(var y = 0; y < 50; y++)
+        {
+            if(x+y <= 50 && 6*x + 4*y <= 240 && 500*x +400*y >= benefice_max)
+            {
+                x_sol = x;
+                y_sol = y;
+                benefice_max = 500*x +400*y;
+            }
+        }
+    }
+    alert("La valeur de x_sol est :" + x_sol);
+    alert("La valeur de y_sol est :" + y_sol);
+    alert("La valeur de benefice_max est :" + benefice_max);
+
 }
